0653d8781ab1d2ab80ee35f0da694338f044e319
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-04-29 16:48:17 +0800

    get started typescript

---
+++ test/class.test.ts
@@ -0,0 +1,16 @@
+describe("Class", () => {
+    it("Should can crate class", () => {
+        
+        class Costumer {
+
+        }
+
+        class Order{
+
+        }
+
+        const costumer : Costumer = new Costumer();
+        const order : Order = new Order();
+        
+    })
+})
