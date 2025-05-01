f6d66eb4fc103ac6f79bd4a3befec3adbff55315
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-05-01 22:29:53 +0800

    refactor: initialize properties in Customer class and update test case

--- test/properties.test.ts
+++ test/properties.test.ts
@@ -1,9 +1,9 @@
 describe("Properties", () => {
     
     class Customer{
-        readonly id : number;
-        name: string
-        age? : number
+        readonly id : number = 2;
+        name: string = "Ryuta";
+        age? : number = 10
 
 
         constructor(id : number, name : string, age : number){
@@ -11,16 +11,22 @@ describe("Properties", () => {
             this.name = name;
             this.age = age;
         }
+
+        sayHello(name : string){
+            console.info(`Hello ${name}, my name is ${name}`);
+        }
     }
 
     it("Sholud can have properties", () => {
 
-        const costumer = new Customer(1, "Rafi", 2);
+        const costumer = new Customer(10,"rau", 23);
 
         console.info(costumer);
         console.info(costumer.id)
         console.info(costumer.name);
         console.info(costumer.age);
+
+        costumer.sayHello("Rafi")
 
     })
 })
