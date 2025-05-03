dbd3e9659e2ce67951c1dea78cb377b5727d9ca7
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-05-03 17:58:50 +0800

    test: add initial inheritance tests for Employee, Manager, and Director classes

---
+++ test/inheritance.test.ts
@@ -0,0 +1,37 @@
+describe("Inheritance", () => {
+
+
+
+    // parrent
+    class Employee {
+        name : string;
+
+        constructor(name : string){
+            this.name = name
+        }
+    }
+
+    class Managet extends Employee {
+
+    }
+
+    class Director extends Managet{
+
+    }
+
+    it("should support", () => {
+
+        const employee = new Employee("Rafi");
+        console.info(employee);
+
+        const manager = new Managet("Ryuta");
+        console.info(manager);
+
+        const derector = new Director("Ryuna");
+        console.info(derector);
+
+    })
+
+
+
+})
