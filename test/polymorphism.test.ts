50fba6870933536dcc8d3a32c82506071c9d98a3
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-05-26 11:29:55 +0800

    test: add sayHello function to demonstrate polymorphism with Employee subclasses

--- test/polymorphism.test.ts
+++ test/polymorphism.test.ts
@@ -9,6 +9,10 @@ describe('Polymorphis', () => {
 
     class VicePresident extends Manager{ }
 
+    function sayHello(employee : Employee): void{
+        console.info(`Hello ${employee.name}`);
+    }
+
 
     it("should support", () => {
         let employee : Employee = new Employee('Rafi');
@@ -19,5 +23,9 @@ describe('Polymorphis', () => {
 
         employee = new VicePresident("Rafi");
         console.info(employee);
+
+        sayHello(employee = new Employee("Rafi"));
+        sayHello(employee = new Manager("Rafi"));
+        sayHello(employee = new VicePresident("Rafi"));
     })
 })
