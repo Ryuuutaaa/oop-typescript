86369db62959eb5158b13c213d8cb727a79b36a1
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-05-26 11:15:30 +0800

    feat : update npm and instanceof

--- test/instanceof.test.ts
+++ test/instanceof.test.ts
@@ -1,12 +1,21 @@
 describe("Instance Of", () => {
+
+     class Employee {}
+     class Manager {}
+
+    const budi = new Employee();
+    const eko = new Manager();
+
     it("Should have problem using type of" , () => {
-        class Employee {}
-        class Manager {}
-
-        const budi = new Employee();
-        const eko = new Manager();
-
         console.info(typeof budi);
         console.info(typeof eko);
+    })
+
+    it("should support Instanceoff", () => {
+        expect(budi instanceof Employee).toBe("true");
+        expect(budi instanceof Manager).toBe("false");
+
+        expect(eko instanceof Manager).toBe("true");
+        expect(eko instanceof Employee).toBe("false");
     })
 })
